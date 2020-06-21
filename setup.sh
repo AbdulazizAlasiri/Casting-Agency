7017cf40d53fc656d882578f3b69ad2a0f47bf3e
Abdulaziz Alasiri <Abdulaziz.A.Alasiri@gmail.com>
2020-06-21 22:54:55 +0300

    Changing Some values

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export EXCITED='test'
+export EXCITED='true'
 export  DATABASE_URL='postgres://xlqyadegarimpa:bb192ecf9cd9016f661fbc28af16a86ae28cc9938cce0102031edf6ece336893@ec2-18-232-143-90.compute-1.amazonaws.com:5432/d6mta08gnu23tt'
 
 export AUTH0_DOMAIN='abdulaziz.auth0.com'
