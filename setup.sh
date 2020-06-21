987f51547f0cd1c05da44df68e98841029dc64e2
Abdulaziz Alasiri <Abdulaziz.A.Alasiri@gmail.com>
2020-06-22 00:41:11 +0300

    modified database URL

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export EXCITED='true'
-export  DATABASE_URL='postgres://xlqyadegarimpa:bb192ecf9cd9016f661fbc28af16a86ae28cc9938cce0102031edf6ece336893@ec2-18-232-143-90.compute-1.amazonaws.com:5432/d6mta08gnu23tt'
+export  DATABASE_URL='postgres://jqxhcwztskiwuh:0a377e5d7308387b4dc7dc0614cfaacacfecea5e02c0f0adfe27beacf72438a7@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d3r5bfiqp5ogsi'
 
 export AUTH0_DOMAIN='abdulaziz.auth0.com'
 export AUTH0_CLIENTID='2BEKoaW9JcIl6Yql1VgFX6nvWrmp94CQ'
